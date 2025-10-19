096a02577881681f48e1693afcbedb81e1170fb2
RopeABN <vadimbark216@gmail.com>
2025-10-19 18:13:43 +0200

    LonaNew2

--- scripts/lona.gd
+++ scripts/lona.gd
@@ -4,6 +4,8 @@ const SPEED = 100.0
 
 @onready var body: AnimatedSprite2D = $body
 @onready var top_clothes: AnimatedSprite2D = $top_clothes
+@onready var jeans: AnimatedSprite2D = $jeans
+@onready var head: AnimatedSprite2D = $head
 
 var last_direction: Vector2 = Vector2.DOWN
 
@@ -21,28 +23,44 @@ func _physics_process(delta):
 		if direction.y > 0:
 			body.play("run_down")
 			top_clothes.play("run_down")
+			jeans.play("run_down")
+			head.play("run_down")
 		elif direction.y < 0:
 			body.play("run_up")
 			top_clothes.play("run_up")
+			jeans.play("run_up")
+			head.play("run_up")
 		if direction.x > 0:
 			body.play("run_right")
 			top_clothes.play("run_right")
+			jeans.play("run_right")
+			head.play("run_right")
 		elif direction.x < 0:
 			body.play("run_left")
 			top_clothes.play("run_left")
+			jeans.play("run_left")
+			head.play("run_left")
 	else:
 		if last_direction.y > 0:
 			body.play("idle_down")
 			top_clothes.play("idle_down")
+			jeans.play("idle_down")
+			head.play("idle_down")
 		elif last_direction.y < 0:
 			body.play("idle_up")
 			top_clothes.play("idle_up")
+			jeans.play("idle_up")
+			head.play("idle_up")
 		elif last_direction.x > 0:
 			body.play("idle_right")
 			top_clothes.play("idle_right")
+			jeans.play("idle_right")
+			head.play("idle_right")
 		elif last_direction.x < 0:
 			body.play("idle_left")
 			top_clothes.play("idle_left")
+			jeans.play("idle_left")
+			head.play("idle_left")
 
 	# normalize the directional movement
 	direction = direction.normalized()
